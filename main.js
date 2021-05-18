645d77fea4e7a6fbd4756a7b03f18df7893ff43a
fatz <github@fatz.tw>
2021-05-18 22:46:19 +0800

    classify DDoS and use lodash

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ if (argv.length != 0) {
     dataDir = argv[0];
 }
 
-fs.readdirSync(dataDir).forEach(logFile => {
+fs.readdirSync(dataDir).forEach(async (logFile) => {
     // get absolute path of file
     const filePath = path.resolve(dataDir, logFile);
-    const attack_type = classifier(filePath);
+    const attack_type = await classifier(filePath);
 
-    console.log(`${logFile} is attack ${attack_type}`);
+    console.log(`${logFile}: ${attack_type}`);
 })
 
-// classifier("./data/port_scan.json")
+// classifier("./data/DDoS.json")
